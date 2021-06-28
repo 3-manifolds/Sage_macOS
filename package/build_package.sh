0b0f29a000419c928c4f6df6e4a02eddf2eeea7b
Marc Culler <culler@users.noreply.github.com>
2021-06-27 19:29:18 -0500

    Packages for different versions need different ids.

--- package/build_package.sh
+++ package/build_package.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 source IDs.sh
+PKG_ID=9_4
+VERSION=1.0
 
-pkgbuild --root local_bin --scripts local_bin/scripts --identifier org.computop.SageMath.bin --version 9.3 --install-location /usr/local/bin bin.pkg
+pkgbuild --root local_bin --scripts local_bin/scripts --identifier org.computop.SageMath.$PKG_ID.bin --version $VERSION --install-location /usr/local/bin bin.pkg
 productsign --sign $DEV_ID bin.pkg packages/SageMath_bin.pkg
 
-pkgbuild --root local_share --identifier org.computop.SageMath.share --version 9.3 --install-location /usr/local/share share.pkg
+pkgbuild --root local_share --identifier org.computop.SageMath.$PKG_ID.share --version $VERSION --install-location /usr/local/share share.pkg
 productsign --sign $DEV_ID share.pkg packages/SageMath_share.pkg
 
 productbuild --distribution Distribution --package-path packages --resources resources recommended.pkg
 
-productsign --sign $DEV_ID recommended.pkg Recommended.pkg
+productsign --sign $DEV_ID recommended.pkg Recommended_$PKG_ID.pkg
 
-xcrun altool --notarize-app --primary-bundle-id "SageMath-9.3" --username "marc.culler@gmail.com" --password $ONE_TIME_PASS --file Recommended.pkg
+xcrun altool --notarize-app --primary-bundle-id "SageMath-$VERSION" --username "marc.culler@gmail.com" --password $ONE_TIME_PASS --file Recommended_$PKG_ID.pkg
 
 echo Waiting one minute ...
 sleep 60
 
-xcrun stapler staple Recommended.pkg
+xcrun stapler staple Recommended_$PKG_ID.pkg
