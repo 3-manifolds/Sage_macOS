0f2a59b615b0c777c8b93f54d4a2b134d45ea9ce
Marc Culler <culler@users.noreply.github.com>
2021-09-28 09:07:39 -0500

    Make the package install sagetex.sty

--- package/build_package.sh
+++ package/build_package.sh
@@ -9,6 +9,9 @@ productsign --sign $DEV_ID bin.pkg packages/SageMath_bin.pkg
 pkgbuild --root local_share --identifier org.computop.SageMath.$PKG_ID.share --version $VERSION --install-location /usr/local/share share.pkg
 productsign --sign $DEV_ID share.pkg packages/SageMath_share.pkg
 
+pkgbuild --root local_texlive --identifier org.computop.SageMath.$PKG_ID.texlive --version $VERSION --install-location /usr/local/texlive texlive.pkg
+productsign --sign $DEV_ID texlive.pkg packages/SageMath_texlive.pkg
+
 productbuild --distribution Distribution --package-path packages --resources resources recommended.pkg
 
 productsign --sign $DEV_ID recommended.pkg Recommended_$PKG_ID.pkg
