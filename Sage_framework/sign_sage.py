e07ac79b7a286d487dd747d0f568b6c9c0f0ffe9
Marc Culler <culler@users.noreply.github.com>
2022-12-09 14:21:09 -0600

    Make extra_files exist, but be empty

--- Sage_framework/sign_sage.py
+++ Sage_framework/sign_sage.py
@@ -6,6 +6,8 @@ entitlement_file = 'entitlement.plist'
 framework_path = os.path.abspath('build/Sage.framework/Versions/Current')
 file_args = ['codesign', '-v', '-s', DEV_ID, '--timestamp', '--options', 'runtime', '--force',
                  '--entitlements', entitlement_file]
+extra_files = []
+
 if len(sys.argv) == 1 or sys.argv[1] != 'framework':
     with open('files_to_sign') as infile:
         for path in infile.readlines():
