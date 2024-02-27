a1eaa76184b86b1163e668c8212d33ca749ca752
Marc Culler <culler@users.noreply.github.com>
2024-02-27 12:42:34 -0600

    Finally found the command to build the documentation.

--- Sage_framework/build_sage.sh
+++ Sage_framework/build_sage.sh
@@ -92,8 +92,9 @@ PIP_ARGS="install --upgrade --no-user --no-deps --target ${SITE_PACKAGES}"
 venv/bin/python3 -m pip ${PIP_ARGS} cocoserver
 # Build the documentatation.
 # The doc build does not seem to work when done in parallel, so no -j4.
-# make doc-clean doc-uninstall
-make
+pushd src/doc
+make PATH=$SAGE_SYMLINK/venv/bin:$PATH SAGE_ROOT=$SAGE_SYMLINK doc-html--all
+popd
 # Move the repo back where it belongs.
 popd
 mv /var/tmp/sage-$VERSION-current repo/sage
