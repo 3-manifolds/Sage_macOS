059d95797318610c329e5e6a840224125e6402dd
Marc Culler <culler@users.noreply.github.com>
2024-10-30 07:04:23 -0500

    Deal with gatekeeper issues on Arm.

--- TclTk_frameworks/build_tcltk.sh
+++ TclTk_frameworks/build_tcltk.sh
@@ -3,7 +3,9 @@ BASE=`pwd`
 #export CFLAGS="-O2 -mmacosx-version-min=10.9 -mno-avx -mno-avx2 -mno-bmi2"
 export CFLAGS="-O2 -mmacosx-version-min=10.9"
 mkdir -p ${BASE}/dist
-make -j6 -C Tcl/macosx install-embedded SUBFRAMEWORK=1 DESTDIR=${BASE}/dist \
-     DYLIB_INSTALL_DIR=@executable_path/../Frameworks/Tcl.framework/Versions/8.6/Tcl
-make -j6 -C Tk/macosx install-embedded SUBFRAMEWORK=1 DESTDIR=${BASE}/dist \
-     DYLIB_INSTALL_DIR=@executable_path/../Frameworks/Tk.framework/Versions/8.6/Tk
+make -C Tcl/macosx
+make -C Tk/macosx
+#make -j6 -C Tcl/macosx install-embedded SUBFRAMEWORK=1 DESTDIR=${BASE}/dist \
+#     DYLIB_INSTALL_DIR=@rpath/Tcl
+#make -j6 -C Tk/macosx install-embedded SUBFRAMEWORK=1 DESTDIR=${BASE}/dist \
+#     DYLIB_INSTALL_DIR=@rpath/Tk
