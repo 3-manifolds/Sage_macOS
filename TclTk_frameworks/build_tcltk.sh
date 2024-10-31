2fec1efbd1974b7755148a461da2439999f98fe8
Marc Culler <culler@users.noreply.github.com>
2024-10-31 13:10:00 -0500

    TclTK framework build.

--- TclTk_frameworks/build_tcltk.sh
+++ TclTk_frameworks/build_tcltk.sh
@@ -3,8 +3,8 @@ BASE=`pwd`
 #export CFLAGS="-O2 -mmacosx-version-min=10.9 -mno-avx -mno-avx2 -mno-bmi2"
 export CFLAGS="-O2 -mmacosx-version-min=10.9"
 mkdir -p ${BASE}/dist
-make -C Tcl/macosx
-make -C Tk/macosx
+make -j6 -C Tcl/macosx deploy
+make -j6 -C Tk/macosx deploy
 #make -j6 -C Tcl/macosx install-embedded SUBFRAMEWORK=1 DESTDIR=${BASE}/dist \
 #     DYLIB_INSTALL_DIR=@rpath/Tcl
 #make -j6 -C Tk/macosx install-embedded SUBFRAMEWORK=1 DESTDIR=${BASE}/dist \
