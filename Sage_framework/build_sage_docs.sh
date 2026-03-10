a665737d45c7d02f7b0c41e824c2ce2471d59068
Marc Culler <culler@users.noreply.github.com>
2026-03-10 14:18:47 -0500

    Deal with mathjax

--- Sage_framework/build_sage_docs.sh
+++ Sage_framework/build_sage_docs.sh
@@ -16,9 +16,9 @@ echo SAGE_SYMLINK=${SAGE_SYMLINK} > local/var/lib/sage/runpath.sh
 chmod +x local/var/lib/sage/runpath.sh
 #make doc-clean doc-uninstall
 #pushd src/doc
-##export PATH=${SAGE_SYMLINK}/venv/bin:$PATH
-##export SAGE_ROOT=${SAGE_SYMLINK}
-##export MATHJAX_DIR=mathjax
+export PATH=${SAGE_SYMLINK}/local/bin:$PATH
+export SAGE_ROOT=${SAGE_SYMLINK}
+export MATHJAX_DIR=mathjax
 make doc-html
 #popd
 popd
